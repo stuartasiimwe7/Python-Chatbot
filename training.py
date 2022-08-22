c3c786ebdcdddffc0402bb8839b54cae4402c4eb
Stuart Asiimwe <stuartgabriel@ymail.com>
2022-08-23 07:16:32 +0900

    23-08-22

--- training.py
+++ training.py
@@ -73,8 +73,9 @@ train_y = list(training[:,1])
 
 #The Model
 import tensorflow as tf
-from tensorflow.keras.layers import Dense,Input,Activation
-from tensorflow.keras.models import Model
+from keras.layers import Dense,Input,Activation
+from keras.models import Model
+from keras.engine import base_layer 
 
 #model = Sequential()
 input_layer = Input(shape=(len(train_x[0])))
@@ -101,4 +102,49 @@ def bow(sentence, words, show_details=False):
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
-return(np.array(bag))
+    return(np.array(bag))
+
+#creating a data structure to hold user context
+context = {}
+ERROR_THRESHOLD = 0.25
+def classify(sentence):
+    # generate probabilities from the model
+    results = model.predict([bow(sentence, words).reshape((1,-1))])[0]
+    # filter out predictions below a threshold
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    # return tuple of intent and probability
+    return return_list
+
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    # if we have a classification then find the matching intent tag
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # find a tag matching the first result
+                if i['tag'] == results[0][0]:
+                    # set context for this intent if necessary
+                    if 'context_set' in i:
+                        if show_details: print ('context:', i['context_set'])
+                        context[userID] = i['context_set']
+
+#check if this intent is contextual and applies to this user's conversation
+                    if not 'context_filter' in i or \
+                        (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
+                        if show_details: print ('tag:', i['tag'])
+                        val = random.choice(i['responses'])
+                        #speech = Speech(val, lang)
+                        #sox_effects = ("speed", "1.0")
+                        #speech.play(sox_effects)
+                        # a random response from the intent
+                        return print(val)
+    results.pop(0)
+ques = input("Hullo There!: ")
+print(classify(ques))
+response(ques)
